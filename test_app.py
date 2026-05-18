1063aa8e893785c88d042f123db7bb2bdd0a7fb9
ksm <ksmdigit@gmail.com>
2026-05-18 11:11:12 +0000

    feat: update

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import say_hello
 
 class TestApp(unittest.TestCase):
     def test_say_hello(self):
-        self.assertEqual(say_hello("AWS"), "Hello, AWS")
+        self.assertEqual(say_hello("AWS"), "Hello, AWS!")
 
 
 if __name__ == '__main__':
